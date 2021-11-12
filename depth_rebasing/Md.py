05992b1c3bcb26b5ee65234006fdf13841ca6cda
touzarilisa <touzarilisa97@gmail.com>
2021-11-12 21:08:39 +0100

    depth rebasing

--- depth_rebasing/Md.py
+++ depth_rebasing/Md.py
@@ -7,8 +7,11 @@ Created on Fri Nov 12 18:24:27 2021
 
 import numpy as np
 
-def Md (Id,N,gz):
-    
+def normalize (mat):
+    max_value = np.max(mat)
+    return mat/max_value
+
+def Md (Id,N,gz):    
     r=16  # constante
     sigma=0.3 #constante
        
@@ -19,11 +22,11 @@ def Md (Id,N,gz):
     Mmid = np.maximum(1-r*Fd**2, np.zeros_like(Fd))
     Mback = np.maximum(-Fd, np.zeros_like(Fd))
     
-    if gz >-1 or gz<sigma:
+    if gz >-1 and gz<sigma:
         Md = Mfront
-    elif gz >-sigma or gz<sigma:
+    elif gz >-sigma and gz<sigma:
         Md = Mmid
-    elif gz >sigma or gz<1:
+    elif gz >sigma and gz<1:
         Md = Mback
     
     return Md
